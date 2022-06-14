a5a7667749176ba3acddf327bc049653e583bee8
clive <98222803+AAClive@users.noreply.github.com>
2022-06-14 22:09:30 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
-form flask import Flask
+from flask import Flask
 from flask import request
+from flask import render_template
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+  if request.method=="POST":
+      pass
+   return render_template("main.html")
