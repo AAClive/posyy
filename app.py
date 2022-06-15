4b62899e3b0c778582c68e4b3b5816c5b34ce2f3
clive <98222803+AAClive@users.noreply.github.com>
2022-06-15 18:43:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ users = db.Table( 'users', meta,
     Column('user_email', VARCHAR),
 )
  
-@app.route("/",methods=["GET","POST")
+@app.route("/",methods=["GET","POST"])
 def siginup():
     if request.method=="POST":
         user=request.form.get("variable")
